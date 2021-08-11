11da9de23f39baecdca3f00d67e4fe442ccd1c88
AbidaAli100 <abida.ali100@gmail.com>
2021-08-12 00:43:43 +0500

    worked on the ui

--- (News App)/src/main.js
+++ (News App)/src/main.js
@@ -1,11 +1,17 @@
-'use strict';
+const target = document.querySelector("#app");
 
-function counter() {
-  let seconds = 0;
-  setInterval(() => {
-    seconds += 1;
-    document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
-  }, 1000);
-}
-
-counter();
+fetch('https://newsapi.org/v2/everything?q=Apple&from=2021-08-11&sortBy=popularity&apiKey=d70ec7078e5b401e83ae4a9ab0ab9fdd')
+    .then(function(response) {
+        return response.json()
+    }).then(function newsData(obj) {
+        let newsArray = obj.articles;
+        console.log(newsArray);
+        console.log(typeof newsArray);
+        document.getElementById("app").innerHTML = (newsArray.map(news =>
+            `<div>
+            <div id= "images"><img src="${news.urlToImage}"/></div>
+            <div> <h2>${news.title} </h2></div>
+            <div> <h3>${news.description}<h3></div>
+        </div>`
+        ).join(''));
+    })
